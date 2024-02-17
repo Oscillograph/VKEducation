191c2069c6a7f8ab1e2c7660e9c465682c089f04
broscillograph <kvkmai@mail.ru>
2024-02-17 13:56:13 +0300

    actualized names and files

--- Algorithms & Structures/VKEdu02-01.cpp
+++ Algorithms & Structures/VKEdu02-01.cpp
@@ -1,64 +1,67 @@
 #include <iostream>
+/*
+* На вход подается не отсортированный массив целых чисел и некоторое целое число «element».
+* Необходимо написать функцию, которая вернет количество элементов, которые не равны данному числу «element».
+* Возвращаемое значение после работы функции обозначает количество чисел не равные «element».
+
+* Пример 1
+* Входные данные:
+* 9
+* 7 8 1 3 11 3 9 4 3
+* 3
+
+* Выходные данные:
+* 6
+*/
+
 #include <vector>
-
-bool ChangePosition(std::vector<int>& source, int oldPos, int newPos)
-{
-	bool success = false;
-	if ((oldPos > 0) && (oldPos < source.size()) &&
-		(newPos >= 0) && (newPos < source.size()))
-	{
-		int temp = source[oldPos];
-		for (int i = oldPos; i > newPos; i--)
-		{
-			source[i] = source[i - 1];
-		}
-		source[newPos] = temp;
-		success = true;
-	}
-	return success;
-}
-
 int main(int argc, char** argv) 
 {
 	int inputNumber = 0;
+	int outputNumber = 0;
 	
 	// first line - total array size
-	// printf("Array size:\n");
 	std::cin >> inputNumber;
-	
 	std::vector<int> array;
 	array.reserve(inputNumber);
 	array.resize(inputNumber);
 	
 	// second line - the array content
-	// printf("Write an array:\n");
 	for (int i = 0; i < array.size(); i++)
 	{
 		std::cin >> array[i];
 	}
 	
-	// process
-	// printf("Processing.\n");
+	// third line - a number to compare elements with
+	std::cin >> inputNumber;
 	
-	for (int i = 0; i < array.size(); i++)
+	// prepare an array to store numbers
+	std::vector<int> noRepeatsArray;
+	noRepeatsArray.reserve(array.size());
+	
+	// fill the array with no-repeating numbers
+	int lastNumber = -array[0];
+	for (auto it = array.begin(); it != array.end(); ++it)
 	{
-		for (int j = 0; j < i; j++)
+		if (*it != lastNumber)
 		{
-			if (array[i] < array[j])
-			{
-				ChangePosition(array, i, j);
-				// std::swap(array[i], array[j]);
-				break;
-			}
+			lastNumber = *it;
+			noRepeatsArray.push_back(lastNumber);
+		}
+	}
+	
+	// comparing procedure
+	outputNumber = 0;
+	for (auto it = noRepeatsArray.begin(); it != noRepeatsArray.end(); ++it)
+	{
+		if (*it != inputNumber)
+		{
+			outputNumber++;
 		}
 	}
 	
 	// output
-	// printf("Output:\n");
-	for (int i = 0; i < array.size(); i++)
-	{
-		std::cout << array[i] << ' ';
-	}
+	std::cout << outputNumber;
 	
 	return 0;
 }
--- Algorithms & Structures/VKEdu03-01.cpp
+++ Algorithms & Structures/VKEdu03-01.cpp
@@ -1,170 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
-// #include <math.h>
-
-/*
-struct vec2
-{
-	int x = 0;
-	int y = 0;
-};
-
-struct Interval
-{
-	int length = 2;
-	int start = 0;
-	int end = 0;
-	bool sorted = false;
-};
-
-bool MergeSort(std::vector<int>& source)
+bool ChangePosition(std::vector<int>& source, int oldPos, int newPos)
 {
 	bool success = false;
-	
-	int currentLength = 2; // the tiniest interval
-	int temp = 0; // a buffer for one value
-	Interval intervalA, intervalB; // intervals
-	
-	while (currentLength <= source.size())
+	if ((oldPos > 0) && (oldPos < source.size()) &&
+		(newPos >= 0) && (newPos < source.size()))
 	{
-		// general O(log n) part
-		intervalA = {currentLength, 0, currentLength - 1, false};
-		intervalB = {currentLength, currentLength, 2*currentLength - 1, false};
-		printf("length: %i\n", currentLength);
-		
-		int sourceSize = source.size();
-		for (int i = 0; (i * currentLength - sourceSize) <= (2 * currentLength); i+=2)
+		int temp = source[oldPos];
+		for (int i = oldPos; i > newPos; i--)
 		{
-			printf("where we are: %i\n", i * currentLength);
-			// set up intervals
-			intervalA.length = currentLength;
-			intervalA.start = i * currentLength;
-			intervalA.sorted = false;
-			
-			intervalB.length = currentLength;
-			intervalB.start = (i + 1) * currentLength;
-			intervalB.sorted = false;
-			
-			// check if the intervals are in the array - there are several options
-			// 1. We have only left interval which is not full
-			if ((intervalA.start + intervalA.length) > source.size())
-			{
-				intervalA.length = source.size() - intervalA.start;
-				intervalA.end = intervalA.start + intervalA.length - 1;
-				intervalA.sorted = true;
-			}
-			
-			// 2. We have two intervals, the right one is not full
-			if ((intervalB.start + intervalB.length) > source.size())
-			{
-				intervalB.length = source.size() - intervalB.start;
-				intervalB.end = intervalB.start + intervalB.length - 1;
-			}
-			
-			// 3. We have two intervals, both of proper size
-			if (!intervalA.sorted)
-			{
-				for (int i = 0; i < intervalA.length; i++)
-				{
-					if (source[intervalA.start + i] <= source[intervalB.start + i])
-					{
-						// do nothing
-						printf("intervalA: {%i, %i}\n", intervalA.start, intervalA.end);
-					}
-					
-					if (source[intervalA.start + i] > source[intervalB.start + i])
-					{
-						printf("intervalB: {%i, %i}\n", intervalB.start, intervalB.end);
-						// save the value in a temporary location
-						temp = source[intervalB.start + i];
-						// shift everything to the right by 1
-						for (int j = intervalB.end; j > (intervalA.start + i); j++)
-						{
-							source[j] = source[j - 1];
-						}
-						// store the value in its place
-						source[intervalA.start + i] = temp;
-						
-						// update indexes
-						
-						// intervalA.start++;
-						// if ((intervalB.start + i) < intervalB.end)
-						// {
-						//	intervalB.start++;
-						//	intervalB.length--;
-						// }
-						
-					}
-				}
-			}
+			source[i] = source[i - 1];
 		}
-		
-		// prepare the next O(log n) cycle
-		currentLength = 2 * currentLength;
+		source[newPos] = temp;
+		success = true;
 	}
-	
-	success = true;
-	
 	return success;
 }
-*/
-
-
-// classic merge sort
-void MergeSort(std::vector<int>& source, int left, int right)
-{
-	if (left == right)
-	{
-		// arrays of size 1 are sorted already
-		return;
-	}
-	
-	// divide and call recursion
-	int middle = (left + right)/2;
-	MergeSort(source, left, middle);
-	MergeSort(source, middle + 1, right);
-	
-	// sort these arrays
-	int start1 = left;
-	int start2 = middle + 1;
-	
-	std::vector<int> temp; // temporary array
-	temp.resize(right);
-	
-	for (int i = 0; i < (right - left + 1); i++) // pick every element in an array of length (right - left - 1)
-	{
-		if ((start2 > right) || // what's left of the first interval
-			((start1 <= middle) && // we have only one interval
-			 (source[start1] <= source[start2])))  // left number is less than right number
-		{
-			temp[i] = source[start1];
-			start1++;
-		} else {
-			temp[i] = source[start2];
-			start2++;
-		}
-	}
-	
-	// rewrite the source array
-	for (int i = 0; i < (right - left + 1); i++)
-	{
-		source[left + i] = temp[i];
-	}
-}
-
-struct Book
-{
-	std::str
-};
 
 int main(int argc, char** argv) 
 {
 	int inputNumber = 0;
 	
 	// first line - total array size
-	printf("Array size:\n");
+	// printf("Array size:\n");
 	std::cin >> inputNumber;
 	
 	std::vector<int> array;
@@ -172,18 +31,30 @@ int main(int argc, char** argv)
 	array.resize(inputNumber);
 	
 	// second line - the array content
-	printf("Write an array:\n");
+	// printf("Write an array:\n");
 	for (int i = 0; i < array.size(); i++)
 	{
 		std::cin >> array[i];
 	}
 	
 	// process
-	printf("Processing.\n");
-	MergeSort(array, 0, array.size() - 1);
+	// printf("Processing.\n");
+	
+	for (int i = 0; i < array.size(); i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (array[i] < array[j])
+			{
+				ChangePosition(array, i, j);
+				// std::swap(array[i], array[j]);
+				break;
+			}
+		}
+	}
 	
 	// output
-	printf("Output:\n");
+	// printf("Output:\n");
 	for (int i = 0; i < array.size(); i++)
 	{
 		std::cout << array[i] << ' ';
